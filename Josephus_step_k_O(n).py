ecc4c36a261e2c255b563d591fcd17d574f53a0a
Nitin Mishra <geekymishnit@gmail.com>
2019-03-07 21:07:04 +0530

    Create Josephus_step_k_O(n).py

---
+++ Josephus_step_k_O(n).py
@@ -0,0 +1,40 @@
+def josephus(n, k): 
+
+  
+
+      if (n == 1): 
+
+          return 1
+
+      else: 
+
+      
+
+      
+
+          # The position returned by  
+
+          # josephus(n - 1, k) is adjusted 
+
+          # because the recursive call 
+
+          # josephus(n - 1, k) considers 
+
+          # the original position  
+
+          # k%n + 1 as position 1  
+
+          return (josephus(n - 1, k) + k-1) % n + 1
+
+  
+# Driver Program to test above function in O(n)
+
+  
+
+n = 7
+
+k = 3
+
+  
+
+print("The chosen place is ", josephus(n, k)) 
