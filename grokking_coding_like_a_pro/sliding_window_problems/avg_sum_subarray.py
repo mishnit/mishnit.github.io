cb6486c34bc3950d7f1eb70624b4f8c941101a63
Nitin Mishra <geekymishnit@gmail.com>
2019-12-11 14:52:00 +0530

    Update avg_sum_subarray.py

--- grokking_coding_like_a_pro/sliding_window_problems/avg_sum_subarray.py
+++ grokking_coding_like_a_pro/sliding_window_problems/avg_sum_subarray.py
@@ -7,12 +7,14 @@
 
 def find_averages_of_subarrays(K, arr):
   result = []
-  for i in range(len(arr)-K+1):
-    # find sum of next 'K' elements
-    _sum = 0.0
-    for j in range(i, i+K):
-      _sum += arr[j]
-    result.append(_sum/K)  # calculate average
+  windowSum, windowStart = 0.0, 0
+  for windowEnd in range(len(arr)):
+    windowSum += arr[windowEnd]  # add the next element
+    # slide the window, we don't need to slide if we've not hit the required window size of 'k'
+    if windowEnd >= K - 1:
+      result.append(windowSum / K)  # calculate the average
+      windowSum -= arr[windowStart]  # subtract the element going out
+      windowStart += 1  # slide the window ahead
 
   return result
 
