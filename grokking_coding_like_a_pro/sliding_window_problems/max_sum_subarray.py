b684981f3ef14f563d5e88669a3de5ceb6ef122d
Nitin Mishra <geekymishnit@gmail.com>
2019-12-11 14:50:37 +0530

    Create max_sum_subarray.py

---
+++ grokking_coding_like_a_pro/sliding_window_problems/max_sum_subarray.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Example 1:
+# Input: [2, 1, 5, 1, 3, 2], k=3 
+# Output: 9
+# Explanation: Subarray with maximum sum is [5, 1, 3].
+
+# Example 2:
+# Input: [2, 3, 4, 1, 5], k=2 
+# Output: 7
+# Explanation: Subarray with maximum sum is [3, 4].
+
+# Time Complexity: O(N)
+# Space Complexity: O(1) i.e. Algorithm runs in Constant Space
+
+def max_sub_array_of_size_k(k, arr):
+  max_sum , window_sum = 0, 0
+  window_start = 0
+
+  for window_end in range(len(arr)):
+    window_sum += arr[window_end]  # add the next element
+    # slide the window, we don't need to slide if we've not hit the required window size of 'k'
+    if window_end >= k-1:
+      max_sum = max(max_sum, window_sum)
+      window_sum -= arr[window_start]  # subtract the element going out
+      window_start += 1  # slide the window ahead
+  return max_sum
+
+
+def main():
+  print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+  print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])))
+
+main()
