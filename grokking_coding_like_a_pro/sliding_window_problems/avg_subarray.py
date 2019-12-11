67964c9f4e587ba77d98c1c09b2ae6f0d9a50aca
Nitin Mishra <geekymishnit@gmail.com>
2019-12-11 14:27:37 +0530

    Create avg_subarray.py

---
+++ grokking_coding_like_a_pro/sliding_window_problems/avg_subarray.py
@@ -0,0 +1,18 @@
+def find_averages_of_subarrays(K, arr):
+  result = []
+  for i in range(len(arr)-K+1):
+    # find sum of next 'K' elements
+    _sum = 0.0
+    for j in range(i, i+K):
+      _sum += arr[j]
+    result.append(_sum/K)  # calculate average
+
+  return result
+
+
+def main():
+  result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+  print("Averages of subarrays of size K: " + str(result))
+
+
+main()
