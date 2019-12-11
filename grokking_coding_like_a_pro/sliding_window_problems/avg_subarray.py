d78c669de59d22a6a052eeb58c3fe88d518d279d
Nitin Mishra <geekymishnit@gmail.com>
2019-12-11 14:31:14 +0530

    Update avg_subarray.py

--- grokking_coding_like_a_pro/sliding_window_problems/avg_subarray.py
+++ grokking_coding_like_a_pro/sliding_window_problems/avg_subarray.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Input: Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+# Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+
 def find_averages_of_subarrays(K, arr):
   result = []
   for i in range(len(arr)-K+1):
