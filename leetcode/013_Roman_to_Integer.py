c05d926dc2fa71b75e8f7aa193103b01a9b2fae0
Nitin Mishra <geekymishnit@gmail.com>
2020-03-05 19:11:44 +0530

    Update 013_Roman_to_Integer.py

--- leetcode/013_Roman_to_Integer.py
+++ leetcode/013_Roman_to_Integer.py
@@ -1,25 +1,4 @@
 class Solution:
-    # def romanToInt(self, s):
-    #     """
-    #     :type s: str
-    #     :rtype: int
-    #     """
-    #     roman = {'I': 1, 'V': 5, 'X': 10,
-    #              'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    #     result = 0
-    #     last = s[-1]
-    #     for t in reversed(s):
-    #         if t == 'C' and last in ['D', 'M']:
-    #             result -= roman[t]
-    #         elif t == 'X' and last in ['L', 'C']:
-    #             result -= roman[t]
-    #         elif t == 'I' and last in ['V', 'X']:
-    #             result -= roman[t]
-    #         else:
-    #             result += roman[t]
-    #         last = t
-    #     return result
-
     def romanToInt(self, s):
         roman = {'I': 1, 'V': 5, 'X': 10,
                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
@@ -32,4 +11,3 @@ class Solution:
                 total -= 2 * prev
             prev = curr
         return total
-
