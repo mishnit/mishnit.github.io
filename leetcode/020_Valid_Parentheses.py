3724092bbcfcec19c210fe49dd6fdc40a5ba6a89
Nitin Mishra <geekymishnit@gmail.com>
2020-03-05 19:37:16 +0530

    Update 020_Valid_Parentheses.py

--- leetcode/020_Valid_Parentheses.py
+++ leetcode/020_Valid_Parentheses.py
@@ -1,59 +1,13 @@
-# class Solution(object):
-#     def isValid(self, s):
-        
-#
 class Solution:
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if s is None:
-            return True
         stack = []
-        for t in s:
-            if t == ')':
-                try:
-                    current = stack.pop()
-                    if current != '(':
-                        return False
-                except:
-                    return False
-            elif t == '}':
-                try:
-                    current = stack.pop()
-                    if current != '{':
-                        return False
-                except:
-                    return False
-            elif t == ']':
-                try:
-                    current = stack.pop()
-                    if current != '[':
-                        return False
-                except:
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
                     return False
             else:
-                stack.append(t)
-        if len(stack) == 0:
-            return True
-        else:
-            return False
-
-
-    # def isValid(self, s):
-    #     # python replace
-    #     n = len(s)
-    #     if n == 0:
-    #         return True
-    #
-    #     if n % 2 != 0:
-    #         return False
-    #
-    #     while '()' in s or '{}' in s or '[]' in s:
-    #         s = s.replace('{}', '').replace('()', '').replace('[]', '')
-    #
-    #     if s == '':
-    #         return True
-    #     else:
-    #         return False
+                return False
+        return stack == []
