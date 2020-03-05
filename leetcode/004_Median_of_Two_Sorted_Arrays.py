0aa724ebb6194fa54837138d0977f810102c0a4c
Nitin Mishra <geekymishnit@gmail.com>
2020-03-05 18:33:28 +0530

    Update 004_Median_of_Two_Sorted_Arrays.py

--- leetcode/004_Median_of_Two_Sorted_Arrays.py
+++ leetcode/004_Median_of_Two_Sorted_Arrays.py
@@ -1,57 +1,36 @@
 class Solution(object):
-    # def findMedianSortedArrays(self, nums1, nums2):
-    #     """
-    #     :type nums1: List[int]
-    #     :type nums2: List[int]
-    #     :rtype: float
-    #     """
-    #     p1 = p2 = 0
-    #     ls1 = len(nums1)
-    #     ls2 = len(nums2)
-    #     all_nums = []
-    #     median = 0.0
-    #     while p1 < ls1 and p2 < ls2:
-    #         if nums1[p1] < nums2[p2]:
-    #             all_nums.append(nums1[p1])
-    #             p1 += 1
-    #         else:
-    #             all_nums.append(nums2[p2])
-    #             p2 += 1
-    #     if p1 < ls1:
-    #         while p1 < ls1:
-    #             all_nums.append(nums1[p1])
-    #             p1 += 1
-    #     if p2 < ls2:
-    #         while p2 < ls2:
-    #             all_nums.append(nums2[p2])
-    #             p2 += 1
-    #     # print all_nums
-    #     if (ls1 + ls2) % 2 == 1:
-    #         median = all_nums[(ls1 + ls2) / 2]
-    #     else:
-    #         median = 1.0 * (all_nums[(ls1 + ls2) / 2] + all_nums[(ls1 + ls2) / 2 - 1]) / 2
-    #     return median
+  def findMedianSortedArrays(self, A, B):
+    m, n = len(A), len(B)
+    if m > n:
+        A, B, m, n = B, A, n, m
+    if n == 0:
+        raise ValueError
 
-    def findMedianSortedArrays(self, nums1, nums2):
-        # https://discuss.leetcode.com/topic/4996/share-my-o-log-min-m-n-solution-with-explanation
-        # https://discuss.leetcode.com/topic/16797/very-concise-o-log-min-m-n-iterative-solution-with-detailed-explanation
-        ls1, ls2 = len(nums1), len(nums2)
-        if ls1 < ls2:
-            return self.findMedianSortedArrays(nums2, nums1)
-        l, r = 0, ls2 * 2
-        while l <= r:
-            mid2 = (l + r) >> 1
-            mid1 = ls1 + ls2 - mid2
-            L1 = -sys.maxint - 1 if mid1 == 0 else nums1[(mid1 - 1) >> 1]
-            L2 = -sys.maxint - 1 if mid2 == 0 else nums2[(mid2 - 1) >> 1]
-            R1 = sys.maxint if mid1 == 2 * ls1 else nums1[mid1 >> 1]
-            R2 = sys.maxint if mid2 == 2 * ls2 else nums2[mid2 >> 1]
-            if L1 > R2:
-                l = mid2 + 1
-            elif L2 > R1:
-                r = mid2 - 1
-            else:
-                return (max(L1, L2) + min(R1, R2)) / 2.0
+    imin, imax, half_len = 0, m, (m + n + 1) / 2
+    while imin <= imax:
+        i = (imin + imax) / 2
+        j = half_len - i
+        if i < m and B[j-1] > A[i]:
+            # i is too small, must increase it
+            imin = i + 1
+        elif i > 0 and A[i-1] > B[j]:
+            # i is too big, must decrease it
+            imax = i - 1
+        else:
+            # i is perfect
+
+            if i == 0: max_of_left = B[j-1]
+            elif j == 0: max_of_left = A[i-1]
+            else: max_of_left = max(A[i-1], B[j-1])
+
+            if (m + n) % 2 == 1:
+                return max_of_left
+
+            if i == m: min_of_right = B[j]
+            elif j == n: min_of_right = A[i]
+            else: min_of_right = min(A[i], B[j])
+
+            return (max_of_left + min_of_right) / 2.0
 
 
 if __name__ == '__main__':
