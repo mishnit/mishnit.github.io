f75d918c28808dd1f5594f255dd1e73c7981ef9e
Nitin Mishra <geekymishnit@gmail.com>
2020-03-05 15:25:07 +0530

    Update 077_Combinations.py

--- leetcode/077_Combinations.py
+++ leetcode/077_Combinations.py
@@ -1,16 +1,17 @@
 class Solution(object):
-    def combine(self, n, k):
-        def combinations(n,k,ind,ans,res):
+        def combinations(self, n,k,ind,ans,res):
             if k == 0:
                 res.append(ans[:])
                 return
             for i in range(ind,n-k+2):
                 ans.append(i)
-                combinations(n,k-1,i+1,ans,res)
+                self.combinations(n,k-1,i+1,ans,res)
                 ans.pop()
-        res = []
-        combinations(n,k,1,[],res)
-        return res
+        
+        def combine(self, n, k):
+            res = []
+            self.combinations(n,k,1,[],res)
+            return res
     
 if __name__ == "__main__":
     s = Solution()
