370e758cfed75c4ce357570369bfed7c02daa493
Nitin Mishra <geekymishnit@gmail.com>
2020-03-05 16:50:28 +0530

    Update 001_Two_Sum.py

--- leetcode/001_Two_Sum.py
+++ leetcode/001_Two_Sum.py
@@ -5,7 +5,7 @@ class Solution:
             other = target - nums[i]
             if other in seen:
                 return [seen[other], i]
-            seen[nums[i]] = i # this line exexutes as else
+            seen[nums[i]] = i # this line executes as else
         return []
         
 if __name__ == '__main__':
