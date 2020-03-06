06b95bf3ce495856e18adca98b5d369a8fcac28d
Nitin Mishra <geekymishnit@gmail.com>
2020-03-06 11:22:34 +0530

    Update 977_Squares_of_a_Sorted_Array.py

--- leetcode/977_Squares_of_a_Sorted_Array.py
+++ leetcode/977_Squares_of_a_Sorted_Array.py
@@ -1,31 +1,13 @@
 class Solution(object):
-    # def sortedSquares(self, A):
-    #     """
-    #     :type A: List[int]
-    #     :rtype: List[int]
-    #     """
-    #     # Directly sort
-    #     return sorted(x * x for x in A) 
-
-    def sortedSquares(self, A):
-        pos = 0
-        while pos < len(A) and A[pos] < 0:
-            pos += 1
-        # pos point to first positve
-        # npos point to larget negative
-        npos = pos - 1
-        res = []
-        while pos < len(A) and npos >= 0:
-            if A[npos] ** 2 < A[pos] ** 2:
-                res.append(A[npos] ** 2)
-                npos -= 1
-            else:
-                res.append(A[pos] ** 2)
-                pos +=1 
-        while npos >= 0:
-            res.append(A[npos] ** 2)
-            npos -= 1
-        while pos < len(A):
-            res.append(A[pos] ** 2)
-            pos += 1
-        return res
+  def sortedSquares(self, A):
+    answer = [0] * len(A)
+    l, r = 0, len(A) - 1
+    while l <= r:
+        left, right = abs(A[l]), abs(A[r])
+        if left > right:
+            answer[r - l] = left * left
+            l += 1
+        else:
+            answer[r - l] = right * right
+            r -= 1
+    return answer
