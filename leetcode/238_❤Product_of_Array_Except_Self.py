9f7940749821681fcdaedaccb3164c40c72f5988
Nitin Mishra <geekymishnit@gmail.com>
2020-04-17 21:51:09 +0530

    Hint: Doing it in O(n) that too without division will require 2 for loops, forward and backward multiplication as dynamic programming

--- leetcode/238_❤Product_of_Array_Except_Self.py
+++ leetcode/238_❤Product_of_Array_Except_Self.py
@@ -1,14 +1,20 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         ans = [1] * len(nums)
         for i in range(1, len(nums)):
             ans[i] = ans[i - 1] * nums[i - 1]
         right = 1
         for i in range(len(nums) - 1, -1, -1):
-            ans[i] *= right
-            right *= nums[i]
+            ans[i] = ans[i] * right
+            right = right * nums[i]
         return ans
+'''
+Input:  [1,2,3,4]
+
+Dynamic Programming trick:
+Initial Output: [1,1,1,1]
+After forward loop: [1,1,2,6]
+After backward loop: [24,12,8,6]
+
+Final Output: [24,12,8,6]
+'''
