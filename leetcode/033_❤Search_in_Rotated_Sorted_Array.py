b546f4940f042aa276cc6cb2a3d15d6a1c2451e7
Nitin Mishra <geekymishnit@gmail.com>
2020-04-20 12:21:44 +0530

    Hint: Modified binary search

--- leetcode/033_❤Search_in_Rotated_Sorted_Array.py
+++ leetcode/033_❤Search_in_Rotated_Sorted_Array.py
@@ -1,27 +1,20 @@
-class Solution:
+class Solution(object):
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        # binary search
-        # if start < mid, then left part is sorted
-        # if mid < end, then right part is sorted
-        def get(start, end):
-          if start > end:
-            return -1
-          mid = (start + end) / 2
-          if nums[mid] == target:
-            return mid
-          elif nums[mid] >= nums[start]: # First half is sorted
-            if target >= nums[start] and target < nums[mid]:
-              return get(start, mid - 1)
-            else:
-              return get(mid + 1, end)
-          elif nums[mid] <= nums[end]: # Second half is sorted
-            if target > nums[mid] and target <= nums[end]:
-              return get(mid + 1, end)
-            else:
-              return get(start, mid - 1)
-        return get(0, len(nums) - 1)
+        l = len(nums) 
+        low = 0
+        high = l-1
+        while low <= high:
+            center = (low + high) // 2
+            if nums[center] == target:
+                return center
+            if nums[low] < nums[center]: #lhs is sorted
+                if nums[low] <= target and (center == 0 or nums[center-1] >= target):
+                    high = center - 1
+                else:
+                    low = center + 1
+            else: #rhs is sorted
+                if (center == l-1 or nums[center+1] <= target) and nums[high] >= target:
+                    low = center + 1
+                else:
+                    high = center - 1               
+        return -1
