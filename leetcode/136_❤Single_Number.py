af420785d846eb00489bb5989bef46e81faf02c2
Nitin Mishra <geekymishnit@gmail.com>
2020-04-22 00:31:23 +0530

    Hint: XOR

--- leetcode/136_❤Single_Number.py
+++ leetcode/136_❤Single_Number.py
@@ -1,7 +1,11 @@
 class Solution(object):
     def singleNumber(self, nums):
-        # xor
         res = 0
         for num in nums:
             res ^= num
         return res
+"""    
+Hint: XOR
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
