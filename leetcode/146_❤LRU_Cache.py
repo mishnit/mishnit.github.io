bdb07d494a2242ee6b2d174d92569d07df392612
Nitin Mishra <geekymishnit@gmail.com>
2020-04-26 20:00:49 +0530

    Hint: Use one queue and one dictionary for maintaining LRU cache

--- leetcode/146_❤LRU_Cache.py
+++ leetcode/146_❤LRU_Cache.py
@@ -21,7 +21,7 @@ class LRUCache:
         else:
             return -1
 
-    def set(self, key, value):
+    def put(self, key, value):
         """
         :type key: int
         :type value: int
@@ -37,23 +37,7 @@ class LRUCache:
         self.cache[key] = value
         self.queue.insert(0, key)
 
-    # def __init__(self, capacity):
-    #     self.dic = collections.OrderedDict()
-    #     self.remain = capacity
-    #
-    # def get(self, key):
-    #     if key not in self.dic:
-    #         return -1
-    #     v = self.dic.pop(key)
-    #     self.dic[key] = v  # set key as the newest one
-    #     return v
-    #
-    # def set(self, key, value):
-    #     if key in self.dic:
-    #         self.dic.pop(key)
-    #     else:
-    #         if self.remain > 0:
-    #             self.remain -= 1
-    #         else:  # self.dic is full
-    #             self.dic.popitem(last=False)
-    #     self.dic[key] = value
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
