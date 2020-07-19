781a1220353d70ffb4bab536fa81afa5b5c9500f
Nitin Mishra <geekymishnit@gmail.com>
2020-07-19 10:29:57 +0530

    Update 1521_closest_to_target.py

--- leetcode/1521_closest_to_target.py
+++ leetcode/1521_closest_to_target.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/find-a-value-of-a-mysterious-function-closest-to-target/
 
-class Solution:
-    def closestToTarget(self, arr: List[int], target: int) -> int:
+class Solution(object):
+    def closestToTarget(self, arr, target):
         a = set([arr[0]])
         stk = set([arr[0]])
         for i in range(1,len(arr)):
@@ -9,9 +9,9 @@ class Solution:
             tmp.add(arr[i])
             a.add(arr[i])
             for j in stk:
-                _ = j&arr[i]
-                tmp.add(_)
-                a.add(_)
+                x = j&arr[i]
+                tmp.add(x)
+                a.add(x)
         ans = abs(target-1000000000)
         for i in a:
             if abs(i- target) < ans:
