a02e31710d5381576a1592ec68e5b804e04d246c
Nitin Mishra <geekymishnit@gmail.com>
2020-07-19 11:12:35 +0530

    Update 1520_max_num_substrings.py

--- leetcode/1520_max_num_substrings.py
+++ leetcode/1520_max_num_substrings.py
@@ -1,46 +1,29 @@
 https://leetcode.com/problems/maximum-number-of-non-overlapping-substrings/
 
-import heapq
-
-class Solution:
+class Solution(object):
     def maxNumOfSubstrings(self, s):
-        first = {}
-        last = {}
-        for i in range(len(s)):
-            c = s[i]
-            last[c] = i
-        for i in range(len(s)-1,-1,-1):
-            c = s[i]
-            first[c] = i
-        intervals = set()
-        lookup = {}
-        for c in first:
-            st = first[c]
-            ed = last[c]
-            visible = set()
-            for i in range(st,ed+1):
-                visible.add(s[i])
-            lookup[c] = visible
-        for c in first:
-            seen = set()
-            def dfs(cur,seen):
-                seen.add(cur)
-                for n in lookup[cur]:
-                    if n not in seen:
-                        dfs(n,seen)
-            dfs(c,seen)
-            lower = min(first[c] for c in seen)
-            upper = max(last[c] for c in seen)
-            intervals.add((lower, upper))
-        pq = []
-        intervals = list(intervals)
-        intervals.sort(key = lambda x : x[1] - x[0])
-        ans = []
-        taken = set()
-        for a,b in intervals:
-            if any(x in taken for x in range(a,b+1)):
-                continue
-            taken.add(a)
-            taken.add(b)
-            ans.append(s[a:b+1])
-        return ans
+	    tmp = {c: [s.index(c), s.rindex(c)+1] for c in set(s)}
+	    # find all the correct boundries
+	    pairs = []
+	    for c in set(s):
+		    l = tmpl = s.index(c)
+		    r = tmpr = s.rindex(c) + 1
+		    while True:
+			    t = set(s[tmpl:tmpr])
+			    for k in t:
+				    tmpl = min(tmpl, tmp[k][0])
+				    tmpr = max(tmpr, tmp[k][1])
+			    if (tmpl, tmpr) == (l, r):
+				    break
+			    l, r = tmpl, tmpr
+		    pairs.append([l, r])
+
+	    # greedy find the optimal solution
+	    # similar to find the maximum number of meetings
+	    pairs.sort(key=lambda x: x[1])
+	    res, last = [], 0
+	    for b, e in pairs:
+		    if b >= last:
+			    res.append(s[b:e])
+			    last = e
+	    return res
