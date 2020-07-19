197814eebd7ab5067826936445209f9b1e02e783
Nitin Mishra <geekymishnit@gmail.com>
2020-07-19 11:22:38 +0530

    Update 1521_closest_to_target.py

--- leetcode/1521_closest_to_target.py
+++ leetcode/1521_closest_to_target.py
@@ -2,18 +2,9 @@
 
 class Solution(object):
     def closestToTarget(self, arr, target):
-        min_diff = abs(target - arr[0])
-        print(min_diff)
-        trials = set()
+        s, ans = set(), float('inf')
         for a in arr:
-            if not min_diff:
-                return 0
-            trials.add(a)
-            tn = set()
-            for t in trials:
-                ta = t & a
-                min_diff = min(min_diff, abs(target - ta))
-                if ta > target:
-                    tn.add(ta)
-            trials = tn
-        return min_diff
+            s = {a & b for b in s} | {a}
+            for a in s:
+                ans = min(ans, abs(a - target))
+        return ans
