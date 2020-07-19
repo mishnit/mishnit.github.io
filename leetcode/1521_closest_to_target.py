1a4a827630eb9ebceebcbb1b063dab448598671c
Nitin Mishra <geekymishnit@gmail.com>
2020-07-19 10:32:44 +0530

    Update 1521_closest_to_target.py

--- leetcode/1521_closest_to_target.py
+++ leetcode/1521_closest_to_target.py
@@ -2,18 +2,18 @@
 
 class Solution(object):
     def closestToTarget(self, arr, target):
-        a = set([arr[0]])
-        stk = set([arr[0]])
-        for i in range(1,len(arr)):
-            tmp = set()
-            tmp.add(arr[i])
-            a.add(arr[i])
-            for j in stk:
-                x = j&arr[i]
-                tmp.add(x)
-                a.add(x)
-        ans = abs(target-1000000000)
-        for i in a:
-            if abs(i- target) < ans:
-                ans = abs(i - target)
-        return ans
+        min_diff = abs(target - arr[0])
+        print(min_diff)
+        trials = set()
+        for a in arr:
+            if not min_diff:
+                return 0
+            trials.add(a)
+            tn = set()
+            for t in trials:
+                ta = t & a
+                min_diff = min(min_diff, abs(target - ta))
+                if ta > target:
+                    tn.add(ta)
+            trials = tn
+        return min_diff
