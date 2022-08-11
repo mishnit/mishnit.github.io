d9bcd0d6518cc5eddea0d0824699cafa24bdbe1c
Nitin Mishra <geekymishnit@gmail.com>
2022-08-11 18:01:26 +0530

    Update Container_With_Most_Water.py

--- leetcode/Container_With_Most_Water.py
+++ leetcode/Container_With_Most_Water.py
@@ -1,17 +1,29 @@
+# Given heights of the bars inside the container, Return the maximum amount of water a container can store 
+# Tip: 
+
 class Solution(object):
-  def maxArea(self, height):
-    max_area = area = 0
-    left, right = 0, len(height) - 1
-    while left < right:
-        l, r = height[left], height[right]
-        if l < r:
-            area = (right - left) * l
-            while height[left] <= l:
-                left += 1
-        else:
-            area = (right - left) * r
-            while height[right] <= r and right:
-                right -= 1
-        if area > max_area:
-            max_area = area
-    return max_area
+    def maxArea(self, heights):
+        l, r = 0, len(heights) - 1
+        result = 0
+
+        while l < r:
+            result = max(result, min(heights[l], heights[r]) * (r - l))
+            if heights[l] < heights[r]:
+                l += 1
+            elif heights[r] <= heights[l]:
+                r -= 1
+        return result
+        
+    
+class Test (object):
+    def testMostWater(self):
+        s = Solution()
+        assert s.maxArea([1,1]) == 1, "Fail"
+        assert s.maxArea([1,8,6,2,5,4,8,3,7]) == 49, "Fail"
+        print "everything passed"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testMostWater()
+
+# O(n)
