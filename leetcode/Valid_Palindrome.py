e3c6db183fbf8ee44100b3a333e0ab7e60c51feb
Nitin Mishra <geekymishnit@gmail.com>
2022-08-11 14:47:07 +0530

    Update Valid_Palindrome.py

--- leetcode/Valid_Palindrome.py
+++ leetcode/Valid_Palindrome.py
@@ -1,15 +1,28 @@
+# Check if given string is a palindrome
+# Tip: Remove all non alphanumeric characters. Find mid of the string and check if all prev and next to mid are matching
+
 class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        alnum_s = [t.lower() for t in s if t.isalnum()]
-        ls = len(alnum_s)
-        if ls <= 1:
+    def isPalindrome(self, str):
+        alnum_str = [t.lower() for t in str if t.isalnum()]
+        if len(alnum_str) <= 1:
             return True
-        mid = ls / 2
+        mid = len(alnum_str) / 2
         for i in range(mid):
-            if alnum_s[i] != alnum_s[ls - 1 - i]:
+            if alnum_str[i] != alnum_str[len(alnum_str) - 1 - i]:
                 return False
         return True
+
+    
+class Test (object):
+    def testIsPalindrome(self):
+        s = Solution()
+        assert s.isPalindrome("") == True, "Fail"
+        assert s.isPalindrome("A man, a plan, a canal: Panama") == True, "Fail" # "amanaplanacanalpanama"
+        assert s.isPalindrome("race a car") == False, "Fail"
+        print "everything passed"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testIsPalindrome()
+
+# O(n)
