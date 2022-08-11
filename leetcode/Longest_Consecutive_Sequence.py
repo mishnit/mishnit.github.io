95125f5f6ee4b82abea3a2f1399f09097f7f87cd
Nitin Mishra <geekymishnit@gmail.com>
2022-08-11 14:30:54 +0530

    Update Longest_Consecutive_Sequence.py

--- leetcode/Longest_Consecutive_Sequence.py
+++ leetcode/Longest_Consecutive_Sequence.py
@@ -1,27 +1,30 @@
-class Solution(object):
-    def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+# Tip: use set to find unique elements then check if its start of a new sequence and compute length_so_far for the sequence 
 
-        def longestConsecutive(self, num):
-            # Pop adjacency O(n) and O(n)
-            num = set(num)
-            maxLen = 0
-            while num:
-                n = num.pop()
-                i = n + 1
-                l1 = 0
-                l2 = 0
-                while i in num:
-                    num.remove(i)
-                    i += 1
-                    l1 += 1
-                i = n - 1
-                while i in num:
-                    num.remove(i)
-                    i -= 1
-                    l2 += 1
-                maxLen = max(maxLen, l1 + l2 + 1)
-            return maxLen
+class Solution(object):
+    def lengthLCS(self, nums):
+        numset = set(nums)
+        longest = 0
+
+        for num in nums:
+            # check if its the start of a sequence, reset length_so_far to 1
+            if (num - 1) not in numset:
+                length_so_far = 1
+                while (num + length_so_far) in numset:
+                    length_so_far += 1
+                longest = max(length_so_far, longest)
+        return longest
+
+    
+class Test (object):
+    def testLengthLCS(self):
+        s = Solution()
+        assert s.lengthLCS([100,4,200,1,3,2]) == 4, "Fail"
+        assert s.lengthLCS([0,3,7,2,5,8,4,6,0,1]) == 9, "Fail"
+        print "everything passed"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testLengthLCS()
+
+# O(n)
