c7209dd777996df7e29ca9e3ae95941e7642da89
Nitin Mishra <geekymishnit@gmail.com>
2022-08-11 18:29:06 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.py

--- leetcode/Best_Time_to_Buy_and_Sell_Stock.py
+++ leetcode/Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,18 +1,28 @@
+# Given stock prices, return max profit by doing buy/sell transactions.
+# Tip: choose two prices such that price at later date minus price at older date is maximised
+
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        length = len(prices)
-        if length == 0:
+        if len(prices) == 0:
             return 0
-        max_profit, low = 0, prices[0]
-        for i in range(1, length):
-            if low > prices[i]:
-                low = prices[i]
-            else:
-                temp = prices[i] - low
-                if temp > max_profit:
-                    max_profit = temp
-        return max_profit
+        
+        maxprofit = 0
+        l = 0
+        for r in range(1, len(prices)):
+            if prices[r] < prices[l]:
+                l = r
+            maxprofit = max(maxprofit, prices[r] - prices[l])
+        return maxprofit
+        
+    
+class Test (object):
+    def testMaxProfit(self):
+        s = Solution()
+        assert s.maxProfit([7,1,5,3,6,4]) == 5, "Fail"
+        assert s.maxProfit([7,6,4,3,1]) == 0, "Fail"
+        print "everything passed"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testMaxProfit()
+
