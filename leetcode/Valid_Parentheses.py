5e8982a66a93fe701ffae0f7bdaf7eda8afa695a
Nitin Mishra <geekymishnit@gmail.com>
2022-08-11 20:53:29 +0530

    Easy

--- leetcode/Valid_Parentheses.py
+++ leetcode/Valid_Parentheses.py
@@ -1,13 +1,32 @@
-class Solution:
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine all parenthesis are valid
+# Tip: Use hashmap to create closing to open parenthesis mapping and use stack to push pop
+
+class Solution(object):
     def isValid(self, s):
+        
+        hashmap = { "]":"[", "}":"{", ")":"(" }
         stack = []
-        dict = {"]":"[", "}":"{", ")":"("}
         for char in s:
-            if char in dict.values():
+            if char in hashmap.values():
                 stack.append(char)
-            elif char in dict.keys():
-                if stack == [] or dict[char] != stack.pop():
+            elif char in hashmap.keys():
+                if stack == [] or hashmap[char] != stack.pop():
                     return False
             else:
                 return False
         return stack == []
+        
+    
+class Test (object):
+    def testIsValid(self):
+        s = Solution()
+        assert s.isValid("()") == True, "Fail"
+        assert s.isValid("()[]{}") == True, "Fail"
+        assert s.isValid("(]") == False, "Fail"
+        print "everything passed"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testIsValid()
+    
+# O(n)
