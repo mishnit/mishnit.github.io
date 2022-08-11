1eb41c7710f1d935b025466c9e24a7a8756e154a
Nitin Mishra <geekymishnit@gmail.com>
2022-08-11 10:59:48 +0530

    Update Two_Sum.py

--- leetcode/Two_Sum.py
+++ leetcode/Two_Sum.py
@@ -1,11 +1,22 @@
+# Return indices of two unique numbers whose sum is equal to target (Only one solution exist)
+# Tip: Convert array into tuple using enumerate and use hashmap to track indices of num and target minus num 
+
 class Solution(object):
     def twoSum(self, nums, target):
         d={}
         for i,num in enumerate(nums):
             if target-num in d:
-                return d[target-num], i
+                return [d[target-num], i]
             d[num]=i
+    
+class Test (object):
+    def testtwoSum(self):
+        s = Solution()
+        assert s.twoSum([3, 2, 4], 6) == [1,2], "Fail"
+        assert s.twoSum([2, 7, 11, 15], 9) == [0,1], "Fail"
+        assert s.twoSum([3, 3], 6) == [0,1], "Fail"
+        print "everything passed"
         
-# if __name__ == '__main__':
-#     s = Solution()
-#     print s.twoSum([3, 2, 4], 6)
+if __name__ == '__main__':
+    t = Test()
+    t.testtwoSum()
