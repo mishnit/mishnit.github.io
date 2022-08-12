0c37ed84ecb1d97e3b24c4c173200dd94391c788
Nitin Mishra <geekymishnit@gmail.com>
2022-08-12 15:15:49 +0530

    medium

--- leetcode/Remove_Nth_Node_From_End_of_List.py
+++ leetcode/Remove_Nth_Node_From_End_of_List.py
@@ -1,4 +1,32 @@
-class Solution(object):
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+class LLNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def insertNode(self, data):
+        if self.val:
+            current = self
+            while(current.next):
+                current = current.next
+            current.next = LLNode(data)
+        else:
+            self = LLNode(data)
+            
+    def insertNodes(self, datas):
+        for data in datas:
+                self.insertNode(data)
+        
+    def printLL(self):
+        out =[]
+        current = self
+        while(current):
+            out.append(current.val)
+            current = current.next
+        return out
+    
+class Solution:
     def removeNthFromEnd(self, head, n):
         if head is None:
             return None
@@ -14,3 +42,29 @@ class Solution(object):
         curr = slow.next
         slow.next = curr.next
         return head
+    
+class Test (object):
+    def testRemoveNthFromEnd(self, LL_data, n, output):
+        if len(LL_data)!=0 and n<=len(LL_data):
+            LL = LLNode(LL_data[0])
+            if len(LL_data)>0:
+                LL.insertNodes(LL_data[1:])
+            s = Solution()
+            LLnew = s.removeNthFromEnd(LL,n)
+            if LLnew == None:
+                assert []==output, "Fail"
+            else:    
+                assert LLnew.printLL()==output, "Fail"
+        else:
+            assert LL_data == output, "Fail"
+        
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testRemoveNthFromEnd([1,2,3,4,5], 2, [1,2,3,5])
+    t.testRemoveNthFromEnd([1], 1, [])
+    t.testRemoveNthFromEnd([1,2], 1, [1])
+    print "everything passed"
+    
+    
+# O(n)
