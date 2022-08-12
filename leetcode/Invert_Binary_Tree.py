a71c49c4e52594abeb4c56321ee004d5c4e7112e
Nitin Mishra <geekymishnit@gmail.com>
2022-08-12 11:14:50 +0530

    Easy

--- leetcode/Invert_Binary_Tree.py
+++ leetcode/Invert_Binary_Tree.py
@@ -1,35 +1,69 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# Given the root of a binary tree, invert the tree, and return its root.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def insertNode(self, data):
+        if self.val:
+            if data < self.val:
+                if self.left is None:
+                    self.left = TreeNode(data)
+                else:
+                    self.left.insertNode(data)
+            elif data > self.val:
+                    if self.right is None:
+                        self.right = TreeNode(data)
+                    else:
+                        self.right.insertNode(data)
+            else:
+                self.val = data
+
+    def insertNodes(self, datas):
+        for data in datas:
+            self.insertNode(data)
 
 class Solution(object):
-    # def invertTree(self, root):
-    #     """
-    #     :type root: TreeNode
-    #     :rtype: TreeNode
-    #     """
-    #     # recursively
-    #     if root is None:
-    #         return None
-    #     right = self.invertTree(root.right)
-    #     left = self.invertTree(root.left)
-    #     root.left = right
-    #     root.right = left
-    #     return root
-
     def invertTree(self, root):
-        # iteratively
-        if root is None:
+        if not root:
             return None
-        queue = [root]
-        while len(queue):
-            curr = queue.pop(0)
-            curr.left, curr.right = curr.right, curr.left
-            if curr.left is not None:
-                queue.append(curr.left)
-            if curr.right is not None:
-                queue.append(curr.right)
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
+    
+    def printLevelOrder(self, root): #Breadth First Traversal
+        if root is None:
+            return
+        queue = []
+        out=[]
+        queue.append(root)
+        while(len(queue) > 0):
+            out.append(queue[0].val)
+            node = queue.pop(0)
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+        return out
+    
+class Test (object):
+    def testInvertTree(self, Actual_Tree_Data, Invert_Tree_Data):
+        root = TreeNode(Actual_Tree_Data[0])
+        root.insertNodes(Actual_Tree_Data[1:])
+        s= Solution()
+        root = s.invertTree(root)
+        assert s.printLevelOrder(root) == Invert_Tree_Data, "Fail"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testInvertTree([4,2,7,1,3,6,9], [4,7,2,9,6,3,1])
+    t.testInvertTree([2,1,3], [2,3,1])
+    print "everything passed"
+    
+# O(n)
