3687c1d7d8df9aa4fae378ccaf0a77d954efa9f5
Nitin Mishra <geekymishnit@gmail.com>
2022-08-13 08:53:01 +0530

    easy

--- leetcode/Meeting_Rooms.py
+++ leetcode/Meeting_Rooms.py
@@ -1,37 +1,31 @@
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+# Given meeting intervals check if one can attend all the meetings?
+
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
 
 class Solution(object):
-    # def canAttendMeetings(self, intervals):
-    #     """
-    #     :type intervals: List[Interval]
-    #     :rtype: bool
-    #     """
-    #     # if start then count += 1
-    #     # if end then count -= 1
-    #     # if count >= 2, then False
-    #     check = []
-    #     for it in intervals:
-    #         check.append((it.start, True))
-    #         check.append((it.end - 1, False))
-    #     check.sort(key=lambda x : x[0])
-    #     count = 0
-    #     for t in check:
-    #         if t[1]:
-    #             count += 1
-    #             if count > 1:
-    #                 return False
-    #         else:
-    #             count -= 1
-    #     return True
-
     def canAttendMeetings(self, intervals):
         intervals.sort(key=lambda x: x.start)
-        ls = len(intervals)
-        for i in range(ls - 1):
+        for i in range(len(intervals) - 1):
             if intervals[i].end > intervals[i + 1].start:
                 return False
-        return True
+        return True     
+    
+class Test (object):
+    def testCanAttendMeetings(self, slots, output):
+        intervals =[]
+        for slot in slots:
+            interval = Interval(slot[0], slot[1])
+            intervals.append(interval)
+        s = Solution()
+        assert s.canAttendMeetings(intervals)== output, "Fail"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testCanAttendMeetings([[0,20],[5,10],[20,30]], False)
+    t.testCanAttendMeetings([[4,6],[7,10]], True)
+    print "everything passed"
+    
+# O(n)
