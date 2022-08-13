1a7e0f3361d434deb57aeabdf7577eef61d76384
Nitin Mishra <geekymishnit@gmail.com>
2022-08-13 09:49:47 +0530

    easy

--- leetcode/253_Min_Rooms_All_Meetings.py
+++ leetcode/253_Min_Rooms_All_Meetings.py
@@ -1,55 +1,37 @@
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+# Given start and end times of all meeting-ids. find min rooms to accommodate all meetings.
 
-
-# class Solution(object):
-#     def minMeetingRooms(self, intervals):
-#         """
-#         :type intervals: List[Interval]
-#         :rtype: int
-#         """
-#         # If there is no meeting to schedule then no room needs to be allocated.
-#         if not intervals:
-#             return 0
-#         # The heap initialization
-#         free_rooms = []
-#         # Sort the meetings in increasing order of their start time.
-#         intervals.sort(key= lambda x: x.start)
-#         # Add the first meeting. We have to give a new room to the first meeting.
-#         heapq.heappush(free_rooms, intervals[0].end)
-#         # For all the remaining meeting rooms
-#         for i in intervals[1:]:
-#             # If the room due to free up the earliest is free, assign that room to this meeting.
-#             if free_rooms[0] <= i.start:
-#                 heapq.heappop(free_rooms)
-#             # If a new room is to be assigned, then also we add to the heap,
-#             # If an old room is allocated, then also we have to add to the heap with updated end time.
-#             heapq.heappush(free_rooms, i.end)
-#         # The size of the heap tells us the minimum rooms required for all the meetings.
-#         return len(free_rooms)
-
+class Interval(object):
+    def __init__(self, s=0, e=0, p=0):
+        self.start = s
+        self.end = e
+        self.pos = p #adding position as after sorting the slots, the ids of meeting would be lost.
 
 class Solution(object):
-    def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: int
-        """
+    def minRooms(self, intervals):
         timeline = []
         for interval in intervals:
-            # meeting root + 1
             timeline.append((interval.start, 1))
-            # meeting root - 1
             timeline.append((interval.end, -1))
-        # sort by time
         timeline.sort()
         ans = curr = 0
-        # go through timeline
         for _, v in timeline:
             curr += v
-            # max meeting room used at this point
             ans = max(ans, curr)
-        return ans
+        return ans   
+    
+class Test (object):
+    def testMinRooms(self, slots, rooms):
+        intervals =[]
+        for slot in slots:
+            interval = Interval(slot[0], slot[1])
+            intervals.append(interval)
+        s = Solution()
+        assert s.minRooms(intervals) == rooms, "Fail"
+        
+if __name__ == '__main__':
+    t = Test()
+    t.testMinRooms([[0,5], [1,2], [1,10]], 3)
+    t.testMinRooms([[0,5], [1,2], [6,10]], 2)
+    print "everything passed"
+    
+# O(n)
