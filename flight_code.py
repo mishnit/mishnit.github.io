6926ee25bc82de1ced414b8a1d6fe557dfe5ba80
Nitin Mishra <geekymishnit@gmail.com>
2023-05-24 23:13:14 +0530

    Add files via upload

---
+++ flight_code.py
@@ -0,0 +1,96 @@
+import requests
+
+headers = {
+    'authority': 'flights-cb.makemytrip.com',
+    'accept': 'application/json',
+    'accept-language': 'en-GB,en;q=0.9,kn-IN;q=0.8,kn;q=0.7,en-US;q=0.6',
+    'access-control-allow-credentials': 'true',
+    'app-ver': '8.0.0',
+    'currency': 'inr',
+    'device-id': 'c79cabde-fde5-4435-9718-199c8617bdf9',
+    'domain': 'in',
+    'language': 'eng',
+    'lob': 'B2C',
+    'mcid': 'c79cabde-fde5-4435-9718-199c8617bdf9',
+    'mmt-auth': 'MAT10c0f2617b445e22a9219a4c922e227c8c620444793abb647a98e8957711d06e81d384e6c3b9f14aab48eaedd8fc0dc13P',
+    'origin': 'https://www.makemytrip.com',
+    'os': 'Android',
+    'pfm': 'PWA',
+    'referer': 'https://www.makemytrip.com/',
+    'region': 'in',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'src': 'mmt',
+    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+    'x-user-cc': 'IN',
+    'x-user-ip': '125.16.129.65',
+    'x-user-rc': 'NEWDELHI',
+}
+
+params = {
+    'pfm': 'PWA',
+    'lob': 'B2C',
+    'crId': '11114da1381-a503-4cb9-9075-e9f5e03d7f544',
+    'cur': 'INR',
+    'lcl': 'en',
+    'shd': 'true',
+    'cc': 'E',
+    'pax': 'A-1_C-0_I-0',
+    'forwardFlowRequired': 'true',
+    'apiTimeStamp': '1683963761190',
+    'region': 'in',
+    'currency': 'inr',
+    'language': 'eng',
+    'cmpId': '',
+    'it': ''
+}
+
+url = 'https://flights-cb.makemytrip.com/api/search'
+
+def getFlightsByPriceAndDuration(SRC, DST, yyyymmdd):
+    params['it'] = SRC+'-'+DST+'-'+str(yyyymmdd)
+    response = requests.get(url, params=params, headers=headers)
+    jsondata = response.json()
+    hashmap = {}
+    if response.status_code==200 and 'journeyMap' in response.json() and 'cardList' in response.json():
+        for key in jsondata['journeyMap']:
+            hashmap[key] = {}
+            layover_min = 0
+            if  jsondata['journeyMap'][key]['layover'] != "":
+                layover_pre_split = jsondata['journeyMap'][key]['layover'].replace(" Technical stop over", '').split(" Layover")[0].rstrip()
+                if 'h' in layover_pre_split and 'm' in layover_pre_split:
+                    layover_split = layover_pre_split.split("h")
+                    layover_min = (60 * int(layover_split[0])) + int(layover_split[1].lstrip().replace("m", ''))
+                elif 'h' in layover_pre_split: # only hour
+                    layover_min = (60 * int(layover_pre_split.replace("h",'')))
+                else: # only minute
+                    layover_min = int(layover_pre_split.rstrip().replace("m", ""))
+            hashmap[key]["layover"] = int(layover_min)
+            hashmap[key]["duration"] = int(jsondata['journeyMap'][key]['duration']["h"])*60 + int(jsondata['journeyMap'][key]['duration']["m"])
+        for key in jsondata['cardList'][0]:
+            hashmap[key['journeyKeys'][0]]["final_price"] = int(key['finalFare'].lstrip("₹ ").replace(",",""))
+        return hashmap
+
+def filterFlightsByLayover(flight_map, minimum_layover_minutes):
+    for key in list(flight_map):
+        if flight_map[key]["layover"] < minimum_layover_minutes and flight_map[key]["layover"] > 0:
+            del flight_map[key]
+    return flight_map
+
+def sortFlightsByPrice(flight_map):
+    list =[["flight_key", "final_price", "duration_in_minutes", "layover_in_minutes"]]
+    for s in sorted(flight_map, key=lambda x: flight_map[x]["final_price"]):
+        list.append([s, flight_map[s]["final_price"], flight_map[s]["duration"], flight_map[s]["layover"]])
+        if len(list) ==11:
+            return list
+
+def getTop10FlightsSortedByPriceAboveLayoverTime(SRC, DST, yyyymmdd, minimum_layover_time):
+    flights = getFlightsByPriceAndDuration(SRC, DST, yyyymmdd)
+    filtered_flights = filterFlightsByLayover(flights, minimum_layover_time)
+    return sortFlightsByPrice(filtered_flights)
+
+if __name__ == '__main__':
+    flight_data = getTop10FlightsSortedByPriceAboveLayoverTime('DEL', 'BOM', 20230528, 120)
+    for fd in flight_data:
+        print(fd, "\n")
